14765f0818717be310b82360e554806f70966908
OH <wjdghks0316@naver.com>
2023-06-26 18:10:32 +0900

    make InputList

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import styled from "styled-components"
 import Gray from "./components/Background"
+import Input from './components/inputList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +12,15 @@ function App() {
   return (
     <body>
     <Gray>
+      <h2>To Do App</h2>
+        <div class="list">
 
+        </div>
+        <div class="input">
+          <Input></Input>
+        </div>
     </Gray>
     </body>
-
   )
 }
 
---
+++ src/components/inputList.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components"
+
+const ButtonInput = styled.input`
+background: #31B404;
+color: #FFFFFF;
+border: #01DF01;
+margin-left: 10px;
+font-size: 20px;
+padding: 12px 25px;
+value: Add;
+`
+const InputText = styled.input`
+background-color: #D3D3D3;
+font-size: 20px;
+font-weight: 700;
+text-color: #FFFFFF;
+padding: 10px 25px;
+width: 800px
+`
+
+
+function Input(){
+    return(
+        <span>
+            <InputText type ="text" placeholder="input to do List"></InputText>
+            <ButtonInput type = "button" value ="Add"></ButtonInput>
+        </span>
+    )
+}
+
+export default Input
